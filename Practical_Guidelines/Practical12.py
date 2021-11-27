b73330e16aa824442274cb9e82b1b383dd9964a1
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-11-27 09:51:10 +0530

    Added Practical 12 Histogram

--- Practical_Guidelines/Practical12.py
+++ Practical_Guidelines/Practical12.py
@@ -2,3 +2,33 @@
 12. Write a program that makes use of a function to accept a list of n integers
     and displays a histogram.
 """
+
+import matplotlib.pyplot as plt
+
+
+def plot_histogram(data):
+    """
+    Objective: Plot a histogram.
+    Input Parameter: data - a list of n integers
+    Return Value: None
+    """
+    plt.hist(data)
+    plt.xlabel("Frequency")
+    plt.ylabel("Value")
+    plt.title("Histogram")
+    # For setting limits on the x-axis
+    plt.xlim(min(data) - 1, max(data) + 1)
+    plt.show()
+
+
+def main():
+    n = int(input("Enter the number of elements in the list: "))
+    data = []
+    for i in range(n):
+        data.append(int(input("Enter the value: ")))
+    # data = [1, 2, 3, 4, 5]
+    plot_histogram(data)
+
+
+if __name__ == '__main__':
+    main()
