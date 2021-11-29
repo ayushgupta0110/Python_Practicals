3418fd8c5faff76a5ee958247fe0b77fa080fdba
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-11-29 10:06:01 +0530

    Practical to show sine and cosine curves

---
+++ Practical_Guidelines/New folder/q13.1.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import math
+
+def sineCurve():
+    """
+    Objective: To plot sine function
+    Input Parameter: None
+    Return value: None
+    """
+    plt.subplot(2,1,1)
+    degrees = range(0, 360 + 1)
+    sineValues = [math.sin (math.radians (i)) for i in degrees]
+    plt.plot(sineValues)
+    plt.xlabel ('Degree')
+    plt.ylabel('Sine Values')
+    plt.title('Sine Curve')
+    plt.grid()
+
+def cosineCurve ():
+    """
+    Objective: To plot cosine function
+    Input Parameter: None
+    Return Value: None
+    """
+    plt.subplot(2,1,2)
+    degrees = range (0, 360 + 1)
+    cosineValues = [math.cos(math.radians (i)) for i in degrees]
+    plt.plot(cosineValues)
+    plt.xlabel('Degree')
+    plt.ylabel ('Cosine Valves')
+    plt.title('Cosine Curve')
+    plt.grid()
+
+def main():
+    """
+    Objective: To Plot sine and cosine curves
+    Input Parameter: None
+    Return Value: None
+    """
+    sineCurve()
+    cosineCurve()
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
